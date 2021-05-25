af65a60a99a84974625e8faf43b4eedce6ae365f
mcc <costanza+git@gmail.com>
2021-05-25 15:53:03 -0700

    user heroku PORT env var

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -45,6 +46,6 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log(`API server listening on port 3001`);
 });
